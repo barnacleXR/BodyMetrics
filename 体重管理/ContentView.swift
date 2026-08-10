7d1d8f151fa035b762b820ea7e7af2639b4455d5
barnacleXR <syuletyoucry@163.com>
2026-08-10 20:47:49 +0900

    Initial Commit

---
+++ 体重管理/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  体重管理
+//
+//  Created by XR on 2026/08/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
